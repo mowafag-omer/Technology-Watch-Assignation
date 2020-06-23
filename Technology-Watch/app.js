cb2f5e6f1487346c5d35cd244a558dec59427e1a
Itistsuna <46030435+Itistsuna@users.noreply.github.com>
2020-06-23 16:16:49 +0200

    Add files via upload

---
+++ Technology-Watch/app.js
@@ -0,0 +1,6 @@
+/* Quand on ajoute un étudiant ca le stock dans un tableau , idem pour les veilles 
+(Créer un formulaire en html , Créer une liste en js)
+Quand on appuie sur le bouton assigner , le programme assigne aléatoirement 2 élèves et 1 technologie en les supprimant de la liste 
+Quand on appuie sur le bouton rafraichir la liste retourne à 0*/
+
+
